402bb3c73e43bb9fb9eada9a5fc0f8f287750b8c
minho Park <minoflower31@naver.com>
2022-06-11 19:36:17 +0900

    fix: Remove files. because not apply .gitignore.

--- out/production/JavaProgrammingToyProject/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE MEMBER(
-    MEMBER_ID varchar(7),
-    NAME varchar(20) NOT NULL,
-    PHONE_NUMBER varchar(13) NOT NULL,
-    PRIMARY KEY (MEMBER_ID)
-);
--- src/service/CrudMode.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-enum CrudMode {
-    //memberId를 입력받을 때 insert는 id가 중복됐는지에 대한 확인
-    //나머지는 id가 없다면 Exception을 날리기 위한 상수
-
-    SELECT, INSERT, DELETE, UPDATE;
-}
